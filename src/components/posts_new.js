24f9397b78fe61b384dfe5dc81e325155dc7e228
jp <jp.san.gabriel01@gmail.com>
2018-07-26 18:58:36 +0800

    Create PostsNew component which allows user to add a post

---
+++ src/components/posts_new.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+
+class PostsNew extends Component {
+
+  render() {
+    return (
+      <div>
+        PostsNew!
+      </div>
+    );
+  }
+
+}
+
+export default PostsNew;
