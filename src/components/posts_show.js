ff92b270f781626bc39c165d9cd604cb3d47224c
jp <jp.san.gabriel01@gmail.com>
2018-07-27 12:49:53 +0800

    Connect fetched post to the PostsShow component

--- src/components/posts_show.js
+++ src/components/posts_show.js
@@ -1,13 +1,26 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { fetchPosts } from '../actions';
 
 class PostsShow extends Component {
+
+  componentDidMount() {
+    const { id } = this.props.match.params;
+    this.props.fetchPosts();
+  }
+
   render () {
+    posts[this.props.match.params.id];
     return (
       <div>
         Post Show!
       </div>
     );
-  };
+  }
 }
 
-export default PostsShow;
+function mapStateToProps({ posts }, ownProps) { //equivalent to posts = state.posts
+  return { post: posts[ownProps.match.params.id] };
+}
+
+export default connect(mapStateToProps, { fetchPosts })(PostsShow);
